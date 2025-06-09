0627b78f5109376af79e6556c16df9cf4647cf84
smilie17 <angelawambui719@gmail.com>
2025-06-09 14:12:51 +0300

    second commit

--- monolithapplication/models.py
+++ monolithapplication/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class ContactMessage(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.firstname} {self.lastname}'
--- monolithproject/settings.py
+++ monolithproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'monolithproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'monolith-database',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
